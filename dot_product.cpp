fe57d6eb6246a3e51aa98aee3be274ee0379d4e0
alinasser2 <44727847+alinasser2@users.noreply.github.com>
2023-05-09 01:04:01 +0300

    Update dot_product.cpp

--- dot_product.cpp
+++ dot_product.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
     double* x = new double[n];
     double* y = new double[n];
 
-    // Initialize the arrays with some data
+
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
             x[i] =  1;
@@ -25,20 +25,20 @@ int main(int argc, char** argv)
         }
     }
 
-    // Broadcast the arrays to all processes
+    
     double start_bcast = MPI_Wtime();
     MPI_Bcast(x, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(y, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     double end_bcast = MPI_Wtime();
 
-    // Compute the local dot product
+    
     double start_compute = MPI_Wtime();
     double local_result = 0.0;
     for (int i = rank; i < n; i += size) {
         local_result += x[i] * y[i];
     }
 
-    // Compute the global dot product using MPI_Reduce
+    
     double global_result;
     MPI_Reduce(&local_result, &global_result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     double end_compute = MPI_Wtime();
